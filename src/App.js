5ee93ff9c264ff39f48aeb4074ccb2fdddd3f45c
dineshsy <s.ydinesh1177@gmail.com>
2020-11-07 14:19:32 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavigationBar } from './Reusables/Interactive/NavigationBar/NavigationBar';
+
+function App() {
+  return (
+    <>
+      <NavigationBar />
+      <main></main>
+    </>
+  );
+}
+
+export default App;
